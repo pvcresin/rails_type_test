e83f1386afac266fc7e9d941365f08776b5705df
pvcresin <pvcresin0730@gmail.com>
2024-03-10 21:01:48 +0900

    feat: delete question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,12 +21,22 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
-  def destoroy
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
   private

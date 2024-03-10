15d4d3d69b736172fe19a440d796b94bb046783d
pvcresin <pvcresin0730@gmail.com>
2024-03-10 20:44:52 +0900

    feat: create new question

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,33 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.save
+    redirect_to @question
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destoroy
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :name, :content)
+    end
+end
